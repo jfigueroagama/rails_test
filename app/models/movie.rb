840dce8bd459f83950ced54b4656456aff701f76
Josue Figueroa <jfigueroagama@redesvozdatos.com>
2013-07-31 18:46:59 -0500

    First commit

---
+++ app/models/movie.rb
@@ -0,0 +1,74 @@
+# Provides resources from public movie API (formerly http://imdbapi.org/).
+# See http://mymovieapi.com/ for details.
+
+class Movie < ActiveResource::Base
+  self.site = 'http://mymovieapi.com'
+  self.element_name = ''
+  self.collection_name = ''
+
+  alias_attribute :id, :imdb_id
+
+  # Returns an array of movies that match title, default limit 1 (max 10).
+  def self.find_all_by_title(title, limit=1)
+    raise ArgumentError, "max limit on API for find_by_title is 10, #{limit} was provided" if limit > 10
+    result = get('/', :q => title, :limit => limit)
+    if result.is_a?(Hash) && result['error']
+      ExceptionResponse.new result
+    else
+      result.collect{ |x| new(x) }
+    end
+  end
+
+  # Returns single instance by title.
+  def self.find_by_title(title)
+    find_all_by_title(title).first
+  end
+
+  # Override find since this service doesn't implement REST the way Rails expects
+  #def self.find(id)
+  #  result = get('/', :id => id)
+  # if result['error']
+  #   ExceptionResponse.new result
+  # else
+  #   new(result)
+  # end
+  #end
+    
+  # Search methos
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
+    #else
+    #  find(:all)
+    end
+  end
+
+
+
+  # takes json response from service and creates exception
+  # response example:  { "code" => 404, "error" => "Film not found" }
+  class ExceptionResponse
+    attr_reader :code, :message
+    def initialize(response)
+      @code    = response.fetch('code', 'Unknown')
+      @message = response.fetch('error', nil)
+      throw
+    end
+
+    private
+
+    def throw
+      case code
+        when 404
+          raise ActiveResource::ResourceNotFound, self
+        when (400..499)
+          raise ActiveResource::ClientError, self
+        when (500..599)
+          raise ActiveResource::ServerError, self
+        else
+          raise Exception, "Unhandled mymovieapi: #{code}: #{message}"
+      end
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTest::Application.config.secret_key_base = '19af082ae3c3e9fc66961d055c6fbd37c6ce8c7848d74a692b9a776779af203ff732a127c78a726d7e95b61b429e9b2867b93eee4f110fc959857afd90922730'
